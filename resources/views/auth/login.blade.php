0c5b87f24257f8e7a9d19d89c2c1ac8911724562
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-06-12 11:10:08 +0500

    логин  и сss изменился

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,25 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Log In | Adminto - Responsive Admin Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
 
-@section('content')
+    <!-- App css -->
+
+    <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- icons -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="loading authentication-bg authentication-bg-pattern">
     <div class="account-pages my-5">
         <div class="container">
 
@@ -67,4 +86,17 @@
         </div>
         <!-- end container -->
     </div>
-@endsection
+
+    <script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{asset('assets/libs/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/libs/jquery.counterup/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
+
+    <!-- App js -->
+    <script src="{{asset('assets/js/app.min.js')}}"></script>
+
+</body>
+</html>
