0ecc500891b69594931405ce0f5577df35b79479
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-06-19 12:07:31 +0500

    web route обновлено

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::group(['middleware'=>['auth', 'language']], function(){
         Route::get('/search/taxi', [OrderController::class, 'searchTaxi']);
     });
 
-    Route::group(['prefix' => 'offer'], function () {
+    Route::group(['prefix' => 'offers'], function () {
         Route::get('/', [OfferController::class, 'index'])->name('offer.index');
         Route::get('/edit/{id}', [OfferController::class, 'edit'])->name('offer.edit');
         Route::get('/show/{id}', [OfferController::class, 'show'])->name('offer.show');
@@ -67,6 +67,14 @@ Route::group(['middleware'=>['auth', 'language']], function(){
         Route::get('/show/{id}', [ClientController::class, 'show'])->name('client.show');
         Route::delete('/destroy/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
         Route::put('/update/{id}', [ClientController::class, 'update'])->name('client.update');
+    });
+
+    Route::group(['prefix' => 'drivers'], function () {
+        Route::get('/', [DriverController::class, 'index'])->name('driver.index');
+        Route::get('/edit/{id}', [DriverController::class, 'edit'])->name('driver.edit');
+        Route::get('/show/{id}', [DriverController::class, 'show'])->name('driver.show');
+        Route::delete('/destroy/{id}', [DriverController::class, 'destroy'])->name('driver.destroy');
+        Route::put('/update/{id}', [DriverController::class, 'update'])->name('driver.update');
     });
 
     Route::resource('user', UserController::class);
@@ -100,6 +108,7 @@ Route::group(['middleware'=>['auth', 'language']], function(){
         Route::post('/store', [CouponContoller::class, 'store'])->name('coupon.store');
         // Route::get('/', [CouponContoller::class, 'index'])->name('coupon.create');
     });
+
     Route::group(['prefix' => 'role'], function () {
         Route::get('/', [RoleController::class, 'index'])->name('role.index');
         Route::get('create', [RoleController::class, 'create'])->name('role.create');
@@ -110,94 +119,6 @@ Route::group(['middleware'=>['auth', 'language']], function(){
         Route::post('/store', [RoleController::class, 'store'])->name('role.store');
         // Route::get('/', [RoleController::class, 'index'])->name('coupon.create');
     });
+
 });
 
-// Route::controller(OrderController::class)->group(function () {
-//     // Route::get('/', [OrderController, 'index'])->name('order.index');
-//     Route::get('/order/', 'index')->name('order.index');
-//     Route::get('/order/edit/{id}', 'edit')->name('order.edit');
-//     Route::delete('/order/destroy/{id}', 'destroy')->name('order.destroy');
-//     Route::put('/order/update/{id}', 'update')->name('order.update');
-//     // Route::post('/orders', 'store');
-// });
-
-Route::group(['prefix' => 'orders'], function () {
-    Route::get('/', [OrderController::class, 'index'])->name('order.index');
-    Route::get('/edit/{id}', [OrderController::class, 'edit'])->name('order.edit');
-    Route::get('/show/{id}', [OrderController::class, 'show'])->name('order.show');
-    Route::delete('/destroy/{id}', [OrderController::class, 'destroy'])->name('order.destroy');
-    Route::put('/update/{id}', [OrderController::class, 'update'])->name('order.update');
-});
-
-Route::group(['prefix' => 'offers'], function () {
-    Route::get('/', [OfferController::class, 'index'])->name('offer.index');
-    Route::get('/edit/{id}', [OfferController::class, 'edit'])->name('offer.edit');
-    Route::get('/show/{id}', [OfferController::class, 'show'])->name('offer.show');
-    Route::delete('/destroy/{id}', [OfferController::class, 'destroy'])->name('offer.destroy');
-    Route::put('/update/{id}', [OfferController::class, 'update'])->name('offer.update');
-});
-
-Route::group(['prefix' => 'clients'], function () {
-    Route::get('/', [ClientController::class, 'index'])->name('client.index');
-    Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('client.edit');
-    Route::get('/show/{id}', [ClientController::class, 'show'])->name('client.show');
-    Route::delete('/destroy/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
-    Route::put('/update/{id}', [ClientController::class, 'update'])->name('client.update');
-});
-
-Route::group(['prefix' => 'drivers'], function () {
-    Route::get('/', [DriverController::class, 'index'])->name('driver.index');
-    Route::get('/edit/{id}', [DriverController::class, 'edit'])->name('driver.edit');
-    Route::get('/show/{id}', [DriverController::class, 'show'])->name('driver.show');
-    Route::delete('/destroy/{id}', [DriverController::class, 'destroy'])->name('driver.destroy');
-    Route::put('/update/{id}', [DriverController::class, 'update'])->name('driver.update');
-});
-
-Route::get('/register', [AuthController::class, 'registerPage'])->name('register');
-Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
-Route::resource('user', UserController::class);
-// Route::resource('role', RoleController::class);
-Route::resource('cars', CarsController::class);
-Route::resource('car-types', CarTypesController::class);
-Route::resource('class-list', ClassListController::class);
-Route::resource('car-list', CarListController::class);
-
-Route::post('/register-post', [AuthController::class, 'register'])->name('register_post');
-Route::post('/login-post', [AuthController::class, 'login'])->name('login_post');
-
-
-Route::group(['prefix' => 'language'], function () {
-
-    Route::get('/', [LanguageController::class, 'index'])->name('language.index');
-    Route::get('/language/show/{id}', [LanguageController::class, 'show'])->name('language.show');
-    Route::post('/translation/save/', [LanguageController::class, 'translation_save'])->name('translation.save');
-    Route::post('/language/change/', [LanguageController::class, 'changeLanguage'])->name('language.change');
-    Route::post('/env_key_update', [LanguageController::class, 'env_key_update'])->name('env_key_update.update');
-    Route::get('/language/create/', [LanguageController::class, 'create'])->name('languages.create');
-    Route::post('/language/added/', [LanguageController::class, 'store'])->name('languages.store');
-    Route::get('/language/edit/{id}', [LanguageController::class, 'languageEdit'])->name('language.edit');
-    Route::post('/language/update/', [LanguageController::class, 'update'])->name('languages.update');
-    Route::get('/language/delete/{id}', [LanguageController::class, 'languageDestroy'])->name('language.destroy');
-    Route::post('/language/update/value', [LanguageController::class, 'updateValue'])->name('languages.update_value');
-});
-
-
-Route::group(['prefix' => 'coupon'], function () {
-    Route::get('/', [CouponContoller::class, 'index'])->name('coupon.index');
-    Route::get('create', [CouponContoller::class, 'create'])->name('coupon.create');
-    Route::get('/edit/{id}', [CouponContoller::class, 'edit'])->name('coupon.edit');
-    Route::put('/update/{id}', [CouponContoller::class, 'update'])->name('coupon.update');
-    Route::delete('/destroy/{id}', [CouponContoller::class, 'destroy'])->name('coupon.destroy');
-    Route::post('/store', [CouponContoller::class, 'store'])->name('coupon.store');
-    // Route::get('/', [CouponContoller::class, 'index'])->name('coupon.create');
-});
-Route::group(['prefix' => 'role'], function () {
-    Route::get('/', [RoleController::class, 'index'])->name('role.index');
-    Route::get('create', [RoleController::class, 'create'])->name('role.create');
-    Route::get('/edit/{id}', [RoleController::class, 'edit'])->name('role.edit');
-    Route::put('/update/{id}', [RoleController::class, 'update'])->name('role.update');
-    Route::delete('/destroy/{id}', [RoleController::class, 'destroy'])->name('role.destroy');
-    Route::post('/store', [RoleController::class, 'store'])->name('role.store');
-    // Route::get('/', [RoleController::class, 'index'])->name('coupon.create');
-});
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
