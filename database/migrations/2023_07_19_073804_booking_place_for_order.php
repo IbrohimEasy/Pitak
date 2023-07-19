7feba7223c76274c72212a9057ed28c348df1850
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-07-19 16:42:29 +0500

    added booking place

--- database/migrations/2023_07_19_073804_booking_place_for_order.php
+++ database/migrations/2023_07_19_073804_booking_place_for_order.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
          Schema::table('yy_orders', function (Blueprint $table) {
-            $table->integer('email')->nullable();
+            $table->integer('booking_place')->nullable();
         });
     }
 
