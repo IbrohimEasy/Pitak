dede3b192b15750b0eee48ce014c287729e74cd3
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-08-01 16:11:11 +0500

    Для настройки в сервере создан вебсокет

--- resources/views/coupon/index.blade.php
+++ resources/views/coupon/index.blade.php
@@ -52,4 +52,15 @@
         {{-- </div> --}}
     {{-- </div> --}}
 
+<script>
+
+var conn = new WebSocket('ws://127.0.0.1:8090/');
+conn.onopen = function(e) {
+    console.log("Connection established!");
+};
+conn.onmessage = function(e) {
+    console.log(e.data);
+};
+
+</script>
 @endsection
